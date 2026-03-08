9d22edd6e67f75f3d5a36881f763f192d4a4f781
phamthainamacc2-cyber <phamthainamacc2@gmail.com>
2026-03-08 17:31:54 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'cotyphu-v1';
+const FILES = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    const clone = res.clone();
+    caches.open(CACHE).then(c => c.put(e.request, clone));
+    return res;
+  })));
+});
